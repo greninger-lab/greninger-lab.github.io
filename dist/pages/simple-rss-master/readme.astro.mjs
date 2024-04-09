0c518206449fc6054a11f2142877f650acd10ec9
Carlos <64579120+Cave42@users.noreply.github.com>
2024-04-09 13:44:19 -0700

    fix test

--- dist/pages/simple-rss-master/readme.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-export { onRequest } from '../../_empty-middleware.mjs';
-
-const page = () => import('../../chunks/pages/readme_k9-mIdba.mjs');
-
-export { page };
