00b99196b4e00c1b31e4fc292282a6751d5376b6
Carlos <64579120+Cave42@users.noreply.github.com>
2024-04-09 13:49:20 -0700

    fix test

--- dist/pages/rss.xml.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('../chunks/pages/rss_lXCH9LfN.mjs');
-
-export { page };
